cc48a068dc9e2ceb50b71265abee6e003d950ed1
Luis Cardoso <luiscard.queiroz@gmail.com>
2025-03-04 11:51:47 -0300

    busca binaria

---
+++ BuscaBinaria/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
+
+var result = BuscaBinaria(nums, 2);
+Console.WriteLine(result.ToString());
+
+// [ 1, 2, 3, 4, 5 ]
+static int BuscaBinaria(List<int> nums, int valor)
+{
+    int baixo = 0;
+    int alto = nums.Count - 1; 
+
+    while(baixo <= alto)
+    {
+        int meio = (baixo + alto) / 2; 
+        if(valor == nums[meio])
+        {
+            return nums[meio];
+        }
+
+        if(valor < nums[meio])
+        {
+            alto = meio - 1;
+        }
+
+        if(valor > nums[meio])
+        {
+            baixo = meio + 1;
+        }
+    }
+
+    return 0;
+}
