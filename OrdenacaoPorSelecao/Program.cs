d52eb4ab486d5af027a1b8d7a49b429a7775e731
Luis Cardoso <luiscard.queiroz@gmail.com>
2025-03-04 12:24:54 -0300

    ordenação por seleção

---
+++ OrdenacaoPorSelecao/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace OrdenacaoPorSelecao
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 5, 3, 6, 2, 10 };
+            var listaOrdenada = OrdenarSelecao(nums);
+
+            foreach (int num in listaOrdenada)
+            {
+                Console.WriteLine(num.ToString());
+            }
+        }
+
+        static int Menor(List<int> nums)
+        {
+            int menor = nums[0];
+            int indiceMenor = 0;
+
+            for(int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] < menor)
+                {
+                    menor = nums[i];
+                    indiceMenor = i;
+                }
+            }
+
+            return menor;
+        }
+
+        // [5, 3, 6, 2, 10]
+        static List<int> OrdenarSelecao(List<int> nums)
+        {
+            List<int> novaLista = new List<int>(nums.Count);
+            int t = nums.Count;
+
+            for (int i = 0; i < t; i++)
+            {
+                int menor = Menor(nums);
+                novaLista.Add(menor);
+                nums.Remove(menor);
+            }
+
+            return novaLista;
+        }
+    }
+}
