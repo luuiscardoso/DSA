28a5a8015e9033001029f27bfeabf7ff3f98e738
Luis Cardoso <luiscard.queiroz@gmail.com>
2025-03-14 20:25:27 -0300

    implementando grafo direcionado e pesquisa em largura

---
+++ Grafos/Grafo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    public class Grafo<T>
+    {
+        private Dictionary<T, List<T>> _grafo;
+
+        public Grafo()
+        {
+           _grafo = new Dictionary<T, List<T>>();
+        }
+
+        void CriarVertice(T vertice)
+        {
+            if (_grafo.ContainsKey(vertice))
+            {
+                throw new InvalidOperationException($"Elemento {vertice} repetido.");
+            }
+
+            _grafo.Add(vertice, new List<T>());
+        }
+
+        void CriarVertice(T vertice, List<T> vizinhos)
+        {
+            if (_grafo.ContainsKey(vertice))
+            {
+                throw new InvalidOperationException($"Elemento {vertice} repetido.");
+            }
+            
+            _grafo.Add(vertice, vizinhos);
+            foreach (T v in vizinhos)
+            {
+                if(!_grafo.ContainsKey(v)) CriarVertice(v);
+            }
+        }
+
+        //criar conexão entre dois vertices
+        void CriarAresta(T origem, T destino)
+        {
+            if (!_grafo.ContainsKey(origem)) CriarVertice(origem);
+            if (!_grafo.ContainsKey(destino)) CriarVertice(destino);
+
+            if (!_grafo[origem].Contains(destino)) 
+            {
+                _grafo[origem].Add(destino);
+            }
+        }
+
+        bool Remove(T vertice)
+        {
+            if (_grafo.ContainsKey(vertice))
+            {
+                foreach (var item in _grafo.Values)
+                {
+                    item.Remove(vertice);
+                }
+
+                _grafo.Remove(vertice);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TestePesquisaEmLargura/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace TestePesquisaEmLargura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Dictionary<string, List<string>> grafo = new Dictionary<string, List<string>>();
+
+                grafo["voce"] = new List<string> { "alice", "bob", "claire" };
+                grafo["bob"] = new List<string> { "angy", "peggy", "voce" };
+                grafo["alice"] = new List<string> { "peggy" };
+                grafo["claire"] = new List<string> { "tom", "jonny" };
+                grafo["angy"] = new List<string>();
+                grafo["peggy"] = new List<string>();
+                grafo["tom"] = new List<string>();
+                grafo["jonny"] = new List<string>();
+
+                Queue<string> rede = new Queue<string>();
+                List<string> verificados = new List<string>();
+
+                void AddFila(List<string> valorGrafo)
+                {
+                    foreach (var pessoa in valorGrafo)
+                    {
+                        rede.Enqueue(pessoa);
+                    }
+                }
+
+                AddFila(grafo["voce"]);
+
+                while (rede.Count > 0)
+                {
+                    var pessoa = rede.Dequeue();
+
+
+                    if (!verificados.Contains(pessoa))
+                    {
+                        if (EhVendedor(pessoa))
+                        {
+                            Console.WriteLine($"{pessoa} é vendedor de manga");
+                            break;
+                        }
+                        else
+                        {
+                            AddFila(grafo[pessoa]);
+                            verificados.Add(pessoa);
+                        }
+                    }
+                }
+            }
+
+
+            static bool EhVendedor(string pessoa)
+            {
+                char ultimaLetra = pessoa[pessoa.Length - 1];
+                if (ultimaLetra == 'm')
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+           
+        }
+    }
+}
